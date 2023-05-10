3db3232044b5662cab34149399ed409bcc3cf95c
357642 <dusqls76@naver,com>
2023-05-10 13:27:33 +0900

    css, js 정리

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ $(function(){
     $('#langBtn').click(function(){
         url=$('#langList').val();
         location.href=url;
-    })
+    });
     /**
      * @메인비주얼_슬라이드
      */
@@ -26,10 +26,10 @@ $(function(){
         on:{
             "slideChange":function(){
                 if(this.realIndex >= 5){
-                    $('.sc-visual .citizen').addClass('active').siblings().removeClass('active')
+                    $('.sc-visual .citizen').addClass('active').siblings().removeClass('active');
                 }else{
-                    $('.sc-visual .news').addClass('active').siblings().removeClass('active')
-                }
+                    $('.sc-visual .news').addClass('active').siblings().removeClass('active');
+                };
             }
         }
     })
@@ -39,12 +39,12 @@ $(function(){
         }else{
             mainSlide.autoplay.stop();
         }
-        $(this).toggleClass('on')
+        $(this).toggleClass('on');
     })
     $('.sc-visual .group-nav .btn-nav').click(function(){
         idx=$(this).data('index');
         $(this).addClass('active').siblings().removeClass('active');
-        mainSlide.slideToLoop(idx)
+        mainSlide.slideToLoop(idx);
     })
     /**
      * @상단으로올라가는_버튼
@@ -52,14 +52,14 @@ $(function(){
     $(window).scroll(function(){
         curr=$(this).scrollTop();
         if(curr >= 20){
-            $('.fix-btn .btn-top').addClass('show')
+            $('.fix-btn .btn-top').addClass('show');
         }else{
-            $('.fix-btn .btn-top').removeClass('show')
-        }
-    })
+            $('.fix-btn .btn-top').removeClass('show');
+        };
+    });
     $('.fix-btn .btn-top').click(function(){
-        window.scrollTo({top:0,behavior:"smooth"})
-    })
+        window.scrollTo({top:0,behavior:"smooth"});
+    });
     /**
      * @하단배너_슬라이드
      */
@@ -79,37 +79,37 @@ $(function(){
             el:'.fraction',
             type:'fraction'
         }
-    })
+    });
     $('.banner-slide .autoplay').click(function(){
         if($(this).hasClass('on')){
             bannerSlide.autoplay.start();
         }else{
             bannerSlide.autoplay.stop();
-        }
+        };
 
-        $(this).toggleClass('on')
-    })
+        $(this).toggleClass('on');
+    });
     /**
      * @하단연관사이트_버튼
      */
     $('.sc-related .btn-related').click(function(){
         if($(this).hasClass('on')){
-            $('.sc-related .btn-related').removeClass('on').siblings('.sub-area').slideUp()
+            $('.sc-related .btn-related').removeClass('on').siblings('.sub-area').slideUp();
             return false;
-        }
-        $('.sc-related .btn-related').removeClass('on').siblings('.sub-area').slideUp()
-        $(this).addClass('on').siblings('.sub-area').slideDown()
-    })
+        };
+        $('.sc-related .btn-related').removeClass('on').siblings('.sub-area').slideUp();
+        $(this).addClass('on').siblings('.sub-area').slideDown();
+    });
     $('.sc-related .sub-list li:first-child').keydown(function(e){
         keyCode = e.keyCode;
         if(keyCode === 9 && e.shiftKey){
-            $('.sc-related .btn-related').removeClass('on').siblings('.sub-area').slideUp()
-        }
-    })
+            $('.sc-related .btn-related').removeClass('on').siblings('.sub-area').slideUp();
+        };
+    });
     $('.sc-related .sub-list li:last-child').keydown(function(e){
         keyCode = e.keyCode;
         if(keyCode === 9 && !e.shiftKey){
-            $('.sc-related .btn-related').removeClass('on').siblings('.sub-area').slideUp()
-        }
-    })
+            $('.sc-related .btn-related').removeClass('on').siblings('.sub-area').slideUp();
+        };
+    });
 })
