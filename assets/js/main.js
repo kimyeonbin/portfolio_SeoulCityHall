14d98ad067c9d8befa3b6363f1bf11f43bda46f6
357642 <dusqls76@naver,com>
2023-06-24 14:37:07 +0900

    js 정리

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,33 @@
 $(function(){
-    /**
-     * @사이트언어변경_버튼
-     */
+    // 사이트언어변경_버튼
     $('#langBtn').click(function(){
-        url=$('#langList').val();
-        location.href=url;
+        url = $('#langList').val();
+        location.href = url;
     });
-    /**
-     * @메인비주얼_슬라이드
-     */
+    // 메인비주얼_슬라이드
     const mainSlide = new Swiper('.main-slide',{
-        loop:true,
+        loop: true,
         autoplay: {
             delay: 1700,
             disableOnInteraction: false
         },
-        navigation:{
-            nextEl:'.next',
-            prevEl:'.prev'
+        navigation: {
+            nextEl: '.next',
+            prevEl: '.prev'
         },
-        pagination:{
-            el:'.fraction',
-            type:'fraction'
+        pagination: {
+            el: '.fraction',
+            type: 'fraction'
         },
-        on:{
-            "slideChange":function(){
+        on: {
+            "slideChange": function(){
                 if(this.realIndex >= 5){
                     $('.sc-visual .citizen').addClass('active').siblings().removeClass('active');
                 }else{
                     $('.sc-visual .news').addClass('active').siblings().removeClass('active');
                 };
             }
-        }
+        },
     });
     $('.main-slide .autoplay').click(function(){
         if($(this).hasClass('on')){
@@ -46,9 +42,7 @@ $(function(){
         $(this).addClass('active').siblings().removeClass('active');
         mainSlide.slideToLoop(idx);
     });
-    /**
-     * @상단으로올라가는_버튼
-     */
+    // 상단으로올라가는_버튼
     $(window).scroll(function(){
         curr=$(this).scrollTop();
         if(curr >= 20){
@@ -58,27 +52,25 @@ $(function(){
         };
     });
     $('.fix-btn .btn-top').click(function(){
-        window.scrollTo({top:0,behavior:"smooth"});
+        window.scrollTo({top: 0,behavior: "smooth"});
     });
-    /**
-     * @하단배너_슬라이드
-     */
+    // 하단배너_슬라이드
     const bannerSlide = new Swiper('.banner-slide',{
-        slidesPerView:3,
-        spaceBetween:43,
-        loop:true,
+        slidesPerView: 3,
+        spaceBetween: 43,
+        loop: true,
         autoplay: {
             delay: 1700,
             disableOnInteraction: false
         },
-        navigation:{
-            nextEl:'.next',
-            prevEl:'.prev'
+        navigation: {
+            nextEl: '.next',
+            prevEl: '.prev'
         },
-        pagination:{
-            el:'.fraction',
-            type:'fraction'
-        }
+        pagination: {
+            el: '.fraction',
+            type: 'fraction'
+        },
     });
     $('.banner-slide .autoplay').click(function(){
         if($(this).hasClass('on')){
@@ -86,12 +78,9 @@ $(function(){
         }else{
             bannerSlide.autoplay.stop();
         };
-
         $(this).toggleClass('on');
     });
-    /**
-     * @하단연관사이트_버튼
-     */
+    // 하단연관사이트_버튼
     $('.sc-related .btn-related').click(function(){
         if($(this).hasClass('on')){
             $('.sc-related .btn-related').removeClass('on').siblings('.sub-area').slideUp();
